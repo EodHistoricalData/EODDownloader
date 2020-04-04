9c296759187c49b51a8f38995b33859e53762595
Tema <artemsaik@gmail.com>
2020-04-04 17:44:40 +0300

    added historicalDataService and RestSharp library

---
+++ EODLoader/EODLoader/Services/EodHistoricalData/EodHistoricalDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EODLoader.Services.EodHistoricalData
+{
+    public class EodHistoricalDataService : IEodHistoricalDataService
+    {
+        public EodHistoricalDataService()
+        {
+        }
+
+
+    }
+}
